af65d5314dfc5aae57aca44e18d4117c106781d4
Ktzukii <dievissie@gmail.com>
2026-01-04 09:02:37 +0200

    probably a bad idea, but eh.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import requests, os, venv
+import requests, os, venv, gzip
 
-class GitRepo:
+class GitRepo: # lua style. (bootstrap/apioframe-cores reference)
     def __init__(self,user,repo,branch):
         self.__repo_base_url__:str = f"https://raw.githubusercontent.com/{user}/{repo}"
         self.branch = branch
+        import json
+        self.mjson = json
 
     def getFile(self,path:str) -> str:
         r = requests.get(f"{self.__repo_base_url__}/refs/heads/{self.branch}/{path}")
         return r.text
 
     def getJsonFile(self,path:str) -> str:
-        import json
         r = requests.get(f"{self.__repo_base_url__}/refs/heads/{self.branch}/{path}")
-        return json.loads(r.text)
+        return self.mjson.loads(r.text)
 
 repo = GitRepo("Kitzukii","vs.ur.ki11.9065.pr0c355","main")
 meta = repo.getJsonFile("fs.json")
